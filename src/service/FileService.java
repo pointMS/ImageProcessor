ffb11c68d2df3aded6e0d77fe75199a284c3f8c1
pointMS <m.sekacheva@gmail.com>
2020-11-10 14:27:20 +0100

    base status of FileService

--- src/service/FileService.java
+++ src/service/FileService.java
@@ -2,34 +2,11 @@ package service;
 
 import entity.ImageDescriptor;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-
-//Реализовать FileService, который по имени csv файла возвращает список объектов класса ImageDescriptor,
-//который содержит URL картинки и название действия.
-//List<FileDescriptor> readFileDescriptors()
 
 public class FileService {
     public List<ImageDescriptor> readImageDescriptors(String fileName) {
-        List<String> stringList = getStringFromFile(fileName);
-        return stringList.stream().map(this::convertToObject).collect(Collectors.toList());
-    }
-
-    private ImageDescriptor convertToObject(String str) {
-        String[] part = str.split(";");
-        return new ImageDescriptor(part[0], part[1]);
-    }
-
-    private List<String> getStringFromFile(String fileName) {
-        List<String> result = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            result = br.lines().collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+        return Collections.EMPTY_LIST;
     }
 }
